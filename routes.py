b2e4e6bc3b6eff04c1db8005c42ad4b0bb24c822
Kaveen Kumarasinghe <ocfinancesmc@gmail.com>
2023-01-27 16:00:57 -0500

    fix

--- routes.py
+++ routes.py
@@ -2,27 +2,38 @@ import flask
 from flask import Flask
 import sys
 
-app = Flask(name)
+app = Flask(__name__)
 
-
-# make types file later
 class Answer:
-    def int(self, answer_id, answer_text):
+    def __init__(self, answer_id, answer_text):
         self.answer_id = answer_id
         self.answer_text = answer_text
 
 
-@app.route('/answer', methods=['GET'])
-def ask(query):
-    # FAQ service -> get answer
-    print(query)
-    sample_ans = Answer('123', 'sample')
-    return sample_ans
+def build_api_error(error_message):
+    return flask.jsonify({"error": error_message})
 
+def build_answer_response(answer: Answer):
+    return flask.jsonify({"answer_id": answer.answer_id, "answer_text": answer.answer_text})
+
+@app.route('/answer', methods=['GET'])
+def ask():
+    sample_ans = Answer('123', 'sample')
+    return build_answer_response(sample_ans)
 
 @app.route('/adjust', methods=['POST'])
-def adjust(answer_id, adjustment):
-    print(adjustment)
-    print(answer_id, file=sys.stderr)
+def adjust():
+    answer_id = flask.request.args.get('answer_id',None)
+    answer_text = flask.request.args.get('answer_text',None)
+
+    if not answer_id or not answer_text:
+        return build_api_error("Missing answer_id or answer_text")
+
     resp = flask.Response('', 200)
     return resp
+
+def init():
+    app.run(host="0.0.0.0", port=8182)
+
+if __name__ == '__main__':
+    init()
