e8730ebc8e8c63b5a52a88637c6d6999176ec373
Lu Yao Chen <luyao.chen@agilebits.com>
2023-02-27 20:03:53 -0800

    init testcase

---
+++ test_basic.py
@@ -0,0 +1,60 @@
+import io
+import json
+import os
+from pathlib import Path
+from pprint import pprint
+
+from flask import jsonify
+import pytest
+from flask.testing import FlaskClient
+from routes import app
+from werkzeug.datastructures import FileStorage
+
+
+'''
+testflow
+
+- create knowledge base
+- upload
+- compose
+- ask
+'''
+
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+
+def test_flow(client: FlaskClient):
+    resp = client.get('/create')
+    data = json.loads(resp.data.decode('utf-8'))
+    knowledgebase_id = data['knowledgebase_id']
+
+    file_name = 'ece350_manual.pdf'
+    file_path = os.path.join(Path(os.path.dirname(__file__)), file_name)
+    with open(file_path, 'rb') as input_file:
+        input_file_stream = io.BytesIO(input_file.read())
+    data = {
+        'knowledgebase_id': knowledgebase_id,
+        'file': (input_file_stream, file_name),
+        'file_name': file_name
+    }
+
+    resp = client.post('/doc/add', content_type='multipart/form-data', data=data)
+    print(resp.data)
+    # data = json.loads(resp.data.decode('utf-8'))
+    print('resp for doc ', data)
+
+    # resp = client.get('/compose', json={'knowledgebase_id': knowledgebase_id})
+    # data = json.loads(resp.data.decode('utf-8'))
+    # print(data)
+    # test_payload = {
+    #     'knowledgebase_id': knowledgebase_id,
+    #     'query': 'what is this document about?'
+    # }
+    #
+    # resp = client.get('/query', json=test_payload)
+    # data = json.loads(resp.data.decode('utf-8'))
+    # print(data)
+    # print(resp)
