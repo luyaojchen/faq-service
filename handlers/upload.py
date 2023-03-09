704aa720df0fd7b42e44a4883671061897c86d36
Lu Yao Chen <luyao.chen@agilebits.com>
2023-03-09 15:54:00 -0800

    merging

--- handlers/upload.py
+++ handlers/upload.py
@@ -1,10 +1,14 @@
 import tempfile
+
+import llama_index
 from llama_index import SimpleDirectoryReader
+import aiohttp
+from llama_index.readers.web import DEFAULT_WEBSITE_EXTRACTOR
 
 from models.statics_model import ResponseStatics, g_index
 
 
-def upload_handler(knowledgebase_id, file):
+def upload_doc_handler(knowledgebase_id, file):
 
     if not knowledgebase_id:
         return False
@@ -38,5 +42,34 @@ def upload_handler(knowledgebase_id, file):
         file.save(fp)
         doc = SimpleDirectoryReader(input_files=[fp.name]).load_data()
         g_index[knowledgebase_id].add_documents(doc)
+
+    return True
+
+
+async def upload_link_handler(knowledgebase_id, url):
+
+    if not knowledgebase_id:
+        return False
+
+    # Check if knowledgebase exists
+    if knowledgebase_id not in g_index:
+        return False
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status != 200:
+                return False
+
+            if response.headers["Content-Type"] == "application/pdf":
+                data = await response.read()
+                f = tempfile.NamedTemporaryFile(suffix=".pdf", delete=False)
+                f.write(data)
+                f.close()
+                doc = SimpleDirectoryReader(input_files=[f.name]).load_data()
+                g_index[knowledgebase_id].add_documents(doc)
+
+            else:
+                documents = llama_index.BeautifulSoupWebReader(website_extractor=DEFAULT_WEBSITE_EXTRACTOR).load_data(urls=[url])
+                g_index[knowledgebase_id].add_documents(documents)
 
     return True
--- routes.py
+++ routes.py
@@ -9,7 +9,9 @@ from langchain import OpenAI
 from handlers.compose import compose_handler
 from handlers.create import create_handler
 from handlers.query import query_handler
-from handlers.upload import upload_handler
+
+from handlers.upload import upload_doc_handler, upload_link_handler
+from models.knowledgebase_model import Knowledgebase
 from models.statics_model import ResponseStatics, g_index
 
 app = Flask(__name__)
@@ -24,13 +26,25 @@ async def create():
     return ResponseStatics.build_creation_response(create_handler())
 
 
-@app.route('/doc/add', methods=['POST'])
-async def upload():
+
+@app.route('/index/doc/add', methods=['POST'])
+def upload_doc():
     # Extract the knowledgebase_id from the request
     knowledgebase_id = flask.request.form.get('knowledgebase_id')
 
     # Validate the knowledgebase ID
-    if not upload_handler(knowledgebase_id, flask.request.files['file']):
+    if not upload_doc_handler(knowledgebase_id, flask.request.files['file']):
+        return ResponseStatics.build_api_error("Invalid knowledgebase ID or none provided")
+    return ResponseStatics.build_upload_success_message()
+
+
+@app.route('/index/link/add', methods=['POST'])
+async def upload_link():
+    # Extract the knowledgebase_id from the request
+    knowledgebase_id, link = flask.request.form.get('knowledgebase_id'), flask.request.form.get('url')
+
+    # Validate the knowledgebase ID
+    if not await upload_link_handler(knowledgebase_id, link):
         return ResponseStatics.build_api_error("Invalid knowledgebase ID or none provided")
     return ResponseStatics.build_upload_success_message()
 
